ebd085de0a17108def5f713fea36d960f97a2746
alina8031 <al_chibizova@mail.ru>
2021-08-09 19:45:47 +0300

    функция shoot

--- Units.js
+++ Units.js
@@ -21,11 +21,8 @@ class Unit1 {
         //this.range = 20;
     }
     update() {
-        if (this.target === null) {
-            this.x += this.movement;
-        } else {
-            projectiles.push(new Projectile(this.x + 70, this.y + 40);
-        }
+        if (this.target === null) this.x += this.movement;
+        else shoot();
         /*if (frame % 10 === 0) {
             if (this.frameX < this.maxFrame) this.frameX++;
             else this.frameX = this.minFrame;
@@ -48,7 +45,15 @@ class Unit1 {
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = 'black';
         ctx.font = '30px Orbitron';
-        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25)
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+    }
+    shoot() {
+        this.projectiles.push(new Projectile({
+            target: this.target,
+            x: this.x,
+            y: this.y
+        }))
+        if (this.health === 25) this.damage = this.damage * 2.5;
     }
 }
 

ad039a5f7badbaafed9d79bde7913010b23b7028
UsefulTornado <Azarel.url@gmail.com>
2021-08-09 11:58:43 +0300

    Создан вспомогательный файл utils.js, туда перенесена функция для расчёта расстония

--- Towers/Tower1.js
+++ Towers/Tower1.js
@@ -1,8 +1,5 @@
 import BaseTower from './BaseTower.js'
-
-calculateDistance(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
-}
+import { calculateDistance } from '../utils/utils.js'
 
 class Tower1 extends BaseTower {
     constructor(ctx, x, y) {

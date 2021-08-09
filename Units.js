50bfaa6ae81a949df88ad6021bdf228e193ff446
alina8031 <al_chibizova@mail.ru>
2021-08-09 15:32:51 +0300

    конструктор юнита1

---
+++ Units.js
@@ -0,0 +1,13 @@
+class Unit1 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.speed = CONSTANTA; //какое-то число
+        this.movement = this.speed;
+        this.health = 100;
+        this.damage = damage; //какое-то число
+        this.radius = radius;
+    }
+}

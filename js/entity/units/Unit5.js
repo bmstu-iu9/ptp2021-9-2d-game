76d17c039043d3d12daf11958b46b418647f6d25
danila_kn <dak151449@gmail.com>
2021-08-18 19:24:45 +0300

    Юнит 5

---
+++ js/entity/units/Unit5.js
@@ -0,0 +1,56 @@
+import BaseUnit from './BaseUnit.js';
+import Projectile1 from '../projectiles/Projectile1.js';
+import * as Constants from './../../constants.js';
+import { calculateDistance } from './../../utils/utils.js';
+
+export default class Unit5 extends BaseUnit {
+    constructor(game, x, y) {
+        super(game, x, y);
+        this.units = game.units;
+        this.range = 6 * Constants.cellSize;
+        this.cost = 100;
+        this.maxHealth = 25;
+        this.health = this.maxHealth;
+        this.damage = 25;
+        this.shootInterval *= 2;
+        this.totalDamage = 0;
+
+    }
+
+    shoot() {
+        if (new Date - this.lastShotTime >= this.shootInterval) {
+            for (let i = 0, n = this.targets.length; i < n; i++) {
+                this.projectiles.push(new Projectile1(
+                    this.targets[i],
+                    this.x + this.width/2,
+                    this.y + this.height/2,
+                    this.damage));
+
+                this.totalDamage += this.damage;
+            }
+            this.lastShotTime = new Date();
+        }
+
+        if (this.totalDamage > 200) {
+            this.totalDamage = 0;
+            this.useAbility();
+        }
+    }
+
+    useAbility() {
+        if (this.shootInterval - 10 > 150) {
+            this.shootInterval -= 10;
+        }
+    }
+
+
+    draw() {
+        let ctx = this.ctx;
+
+        ctx.fillStyle = 'orange';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'black';
+        ctx.font = Constant.fontSize + 'px Orbitron';
+        ctx.fillText(Math.floor(this.health), this.x + 5, this.y + 15);
+    }
+}

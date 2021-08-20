2215addf030c6e37ff6a41d3feb7f1b80615a1cc
UsefulTornado <Azarel.url@gmail.com>
2021-08-20 19:44:26 +0300

    рефакторинг

--- js/components/mouse.js
+++ js/components/mouse.js
@@ -1,10 +1,13 @@
+import * as Constants from './../constants.js';
+import { chooseTower, putTower } from './towersProcessing.js';
+import { chooseUnit, putUnit } from './unitsProcessing.js';
+
 export default class Mouse {
     constructor(game) {
         this.x = 0;
         this.y =  0;
         this.width = 0.1;
         this.height = 0.1;
-        //this.clicked = false;
         this.canvas = game.canvas;
     }
 
@@ -15,19 +18,39 @@ export default class Mouse {
             game.mouse.x = e.x - canvasPosition.left;
             game.mouse.y = e.y - canvasPosition.top;
         });
-        /*
-        this.canvas.addEventListener('mouseleave', function() {
-            game.mouse.x = undefined;
-            game.mouse.y = undefined;
-        });
 
-        this.canvas.addEventListener('mousedown', function() {
-            game.mouse.clicked = true;
-        });
+        this.canvas.addEventListener('click', function () {
+            let clickLocation = game.mouse.detectClickLocation(game);
 
-        this.canvas.addEventListener('mouseup', function() {
-            game.mouse.clicked = false;
+            if (clickLocation == "Control Bar") {
+                let chosenTower = chooseTower(game.ctx, game.mouse);
+                if (chosenTower == game.chosenTower) {
+                    game.chosenTower = null;
+                } else {
+                    game.chosenTower = chosenTower;
+                }
+
+                let chosenUnit = chooseUnit(game.ctx, game.mouse);
+                if (chosenUnit == game.chosenUnit) {
+                    game.chosenUnit = null;
+                } else {
+                    game.chosenUnit = chosenUnit;
+                }
+            } else if (clickLocation == "Game Grid") {
+                if (game.chosenTower) {
+                    putTower(game);
+                } else if (game.chosenUnit) {
+                    putUnit(game);
+                }
+            }
         });
-        */
+    }
+
+    detectClickLocation(game) {
+        if (game.mouse.y <= Constants.controlBarHeight) {
+            return "Control Bar";
+        } else {
+            return "Game Grid";
+        }
     }
 }
